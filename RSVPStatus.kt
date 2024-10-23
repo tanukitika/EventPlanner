036ac3b969a72224fbc2a07a75b5e58fd39d70af
tanukitika <127783169+tanukitika@users.noreply.github.com>
2024-10-23 18:02:49 -0400

    Create RSVPStatus.kt
    First Iteration of RSVP Status

---
+++ RSVPStatus.kt
@@ -0,0 +1,8 @@
+// RSVPStatus Enum with additional statuses
+enum class RSVPStatus {
+    PENDING,
+    ATTENDING,
+    NOT_ATTENDING,
+    MAYBE,         // New status
+    WAITLISTED     // New status
+}
